2c9d9022b60cf2c91c964a57b743d577841db52d
Victor Silva <victormarques.ia@gmail.com>
2020-11-04 22:46:11 -0300

    feat: create Navbar component

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,4 +1,11 @@
+import React, { ButtonHTMLAttributes } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+
+interface ButtonProps extends React.DetailsHTMLAttributes<ButtonHTMLAttributes<HTMLButtonElement>> {
+  primary: boolean;
+  big?: boolean;
+  fontBig?: boolean;
+}
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -21,6 +28,30 @@ export const Container = styled.div`
   @media screen and (max-width: 991px) {
     padding-right: 30px;
     padding-left: 30px;
+  }
+`;
+
+export const Button = styled.button<ButtonProps>`
+  border-radius: 4px;
+  padding: ${({ big }) => (big ?'12px 64px' : '10px 20px')};
+  white-space: nowrap;
+  
+  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+
+  background: ${({ primary }) => ( primary ? '#4b59f7' : '#0467fb' )};
+  color: #fff;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    transition: all 0.3s ease-out;
+    background: #fff;
+    background: ${({ primary }) => ( primary ? '#0467fb' : '#4b59f7' )};
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
   }
 `;
 
