b2d0e4f6ed9fbed82e483e6a63bfa42017d8cf66
Victor Silva <victormarques.ia@gmail.com>
2020-11-06 22:48:03 -0300

    feat: create scrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import Home from './pages/HomePage/Home';
 import Products from './pages/Products/Products';
 import Services from './pages/Services/Services';
 import SignUp from './pages/SignUp/SignUp';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home}/>
