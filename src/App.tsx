aeea4b3a7dac1c1cad760735e121cdb502d811e3
Victor Silva <victormarques.ia@gmail.com>
2020-11-06 22:44:53 -0300

    feat: create SignUp page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Footer, Navbar } from './components';
 import Home from './pages/HomePage/Home';
 import Products from './pages/Products/Products';
 import Services from './pages/Services/Services';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" exact component={Home}/>
         <Route path='/services' component={Services} />
         <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
       </Switch>
       <Footer />
     </Router>
